5a66296706136a21f5a6162986a8ff2a595e2434
Aiko M <minmilera@gmail.com>
2021-05-04 22:11:56 +0200

    Code refactoring (3b - 5b)

--- L2/js/script.js
+++ L2/js/script.js
@@ -1,8 +1,7 @@
 // Globala variabler
-var input1Elem;
-var input2Elem;
-var msgElem;
-var imgElem;
+var input1Elem, input2Elem; // Referenser till textfälten för indata 
+var msgElem; // Referens till element för fel meddelande
+var imgElem; // Referens till element för frukt bild
 
 // ------------------------------
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
@@ -11,18 +10,31 @@ function init() {
   input1Elem = document.getElementById('input1');
   input2Elem = document.getElementById('input2');
   msgElem = document.getElementById('message');
-  imgElem = document.getElementById('fruitImg');
+  // imgElem = document.getElementById('fruitImg');
   document.getElementById('btn1').onclick = showFruit;
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 // ------------------------------
 
+// ------------------------------
+// Funktion som anropa funktionen som validera numret 
+// som användare har valt. Och sen bytas ut bilden 
+// (om användaren valt numret mellan 1 och 5). 
 function showFruit() {
-  // Läs indata
-  let nr = Number(input1Elem.value);
-  let url; // Variabel
 
-  // Bilders url
+  // Den första paramentern: värdet i textfältet
+  // Den andra paramentern: den övre gränsen för tillåtna värden
+  let nr = checkNr(input1Elem.value, 5);
+
+  // Bilds url byts ut
+  document.getElementById('fruitImg').src = getUrl(nr);
+}
+// ------------------------------
+
+// ------------------------------
+// Får bildens url
+function getUrl(nr) {
+  let url; // Variabel för bilds url
   switch (nr) {
     case 1:
       url = "img/apple.png";
@@ -47,5 +59,28 @@ function showFruit() {
     default:
       url = "img/nofruit.png";
   }
-  imgElem.src = url;  // Byt ut bildens url
+  return url;
+} // End getUrl
+// ------------------------------
+
+// ------------------------------
+function checkNr(nr, high) {
+  msgElem.innerHTML = "";
+
+  // Kontrollera om nummer är ett tal
+  if (isNaN(nr)) {
+    msgElem.innerHTML = "Du måste skriva ett tal med siffror.";
+    return null; // Om numret är fel, returnerar ju funktionen null
+  }
+
+  // Kontrollera att talet ligger utanför intervallet 1 och 5.
+  if (nr < 1 || nr > high) {
+    msgElem.innerHTML = "Du måste skriva ett tal mellan 1 och " + high;
+    return null; // Om numret är fel, returnerar ju funktionen null
+  }
+
+  // Om talet är decimaltal konverteras numret til ett heltal och visa det för användaren
+  nr = parseInt(nr);
+
+  return nr;
 }
